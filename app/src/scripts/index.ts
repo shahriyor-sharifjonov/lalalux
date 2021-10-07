8dc4cba68efcb80eb487bc75873bca9c7a1b4dc8
Shahriyor <shahriyordeveloping@gmail.com>
2021-10-07 18:40:00 +0300

    initial

--- app/src/scripts/index.ts
+++ app/src/scripts/index.ts
@@ -9,21 +9,74 @@ var swiper3 = new Swiper(".intro", {
 });
 
 var swiper = new Swiper(".hot__carousel", {
-    slidesPerView: 4,
+    slidesPerView: 1,
     spaceBetween: 40,
     pagination: {
       el: ".hot__pagination",
       clickable: true,
     },
+    breakpoints: {
+      576: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 40,
+      },
+      992: {
+        slidesPerView: 3,
+        spaceBetween: 40,
+      },
+      1200: {
+        slidesPerView: 4,
+        spaceBetween: 40,
+      },
+    },
   });
 
 var swiper2 = new Swiper(".about__slider", {
-    slidesPerView: 4,
+    slidesPerView: 2,
     spaceBetween: 40,
     autoHeight: true,
     navigation: {
       nextEl: ".about-button-next",
       prevEl: ".about-button-prev",
     },
+    breakpoints: {
+      576: {
+        slidesPerView: 2,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      992: {
+        slidesPerView: 3,
+      },
+      1200: {
+        slidesPerView: 4,
+      },
+    },
   });
 
+// Header Menu
+const headerButton: HTMLButtonElement =
+  document.querySelector(".header__button");
+const headerMenu: HTMLUListElement = document.querySelector(".header__cat");
+let menuOpened = false;
+const menuToggle = () => {
+  menuOpened = !menuOpened;
+  headerButton.classList.toggle("open");
+  headerMenu.classList.toggle("open");
+};
+
+headerButton.onclick = menuToggle;
+
+window.onclick = (e: MouseEvent) => {
+  if (
+    menuOpened &&
+    !e.composedPath().includes(headerButton) &&
+    !e.composedPath().includes(headerMenu)
+  )
+    menuToggle();
+};
